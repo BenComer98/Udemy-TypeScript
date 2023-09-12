06b1d580083029fea17213f9827028364ab65a1d
BenComer98 <benalancomer@gmail.com>
2023-09-12 16:22:44 -0700

    Completed first project

--- 01_networkreq/fetchjson/index.ts
+++ 01_networkreq/fetchjson/index.ts
@@ -14,9 +14,13 @@ axios.get(url).then(response => {
   const title = todo.title;
   const finished = todo.completed;
 
-  console.log(`
-    The Todo with ID: ${ID}
-    has a title of: ${title}
-    Is it finished? ${finished}
-  `);
+  logTodo(ID, title, finished);
 });
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`
+    The Todo with ID: ${id}
+    has a title of: ${title}
+    Is it finished? ${completed}
+  `);
+};
