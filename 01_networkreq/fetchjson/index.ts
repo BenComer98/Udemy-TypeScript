80d68bb467bba7453f6a1b44cceabadd5e715690
BenComer98 <benalancomer@gmail.com>
2023-09-12 16:15:38 -0700

    Project started

---
+++ 01_networkreq/fetchjson/index.ts
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+axios.get(url).then(response => {
+  const todo = response.data as Todo;
+
+  const ID = todo.id;
+  const title = todo.title;
+  const finished = todo.completed;
+
+  console.log(`
+    The Todo with ID: ${ID}
+    has a title of: ${title}
+    Is it finished? ${finished}
+  `);
+});
